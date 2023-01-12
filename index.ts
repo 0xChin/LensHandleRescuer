fbdaba18749855a54baecf3f8fada5076e0b5425
AlanRacciatti <alanracciatti1220@gmail.com>
2023-01-11 21:25:59 -0300

    feat: add working script

--- index.ts
+++ index.ts
@@ -4,11 +4,12 @@ import {
   FlashbotsBundleTransaction,
   FlashbotsBundleResolution,
 } from "@flashbots/ethers-provider-bundle";
+
 require("dotenv").config();
 
 async function main() {
-  // Standard json rpc provider directly from ethers.js (NOT Flashbots)
-  // create the base provider
+  console.log(process.env);
+
   let base = new ethers.providers.JsonRpcProvider(
     { url: process.env.POLYGON_RPC_URL! },
     137
@@ -16,19 +17,19 @@ async function main() {
 
   await base.ready;
 
-  const user = new ethers.Wallet(
+  const sponsorWallet = new ethers.Wallet(
     process.env.NON_COMPROMISED_PRIVATE_KEY!,
     base
   );
 
-  const hackedUser = new ethers.Wallet(
+  const hackedWallet = new ethers.Wallet(
     process.env.COMPROMISED_PRIVATE_KEY!,
     base
   );
 
   let provider = new FlashbotsBundleProvider(
     base,
-    user,
+    sponsorWallet,
     { url: "http://bor.txrelay.marlin.org/" },
     137
   );
@@ -36,60 +37,43 @@ async function main() {
   const CONTRACT_ADDRESS = "0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d"; // Lens Contract https://polygonscan.com/address/0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d#code
 
   const ABI = [
-    {
-      inputs: [
-        { internalType: "address", name: "from", type: "address" },
-        { internalType: "address", name: "to", type: "address" },
-        { internalType: "uint256", name: "tokenId", type: "uint256" },
-      ],
-      name: "safeTransferFrom",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
+    "function safeTransferFrom(address from, address to, uint256 tokenId)",
   ];
 
-  const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, user);
+  const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, hackedWallet);
+  const gasPrice = ethers.utils.parseUnits("1100", "gwei"); // Hardcoded from https://polygas.org/
 
   const txs: FlashbotsBundleTransaction[] = [
     {
-      signer: user,
+      signer: sponsorWallet,
       transaction: {
-        to: user.address,
-        gasPrice: ethers.utils.parseUnits("180", "gwei"),
+        to: hackedWallet.address,
+        gasPrice,
         gasLimit: 21000,
         chainId: 137,
-        value: 0,
+        value: ethers.utils.parseEther("0.5"), // 0.5 MATIC
       },
     },
     {
-      signer: user,
-      transaction: {
-        to: user.address,
-        gasPrice: ethers.utils.parseUnits("180", "gwei"),
-        gasLimit: 21000,
-        chainId: 137,
-        value: 0,
-      },
-    },
-    /*     {
-      signer: user,
+      signer: hackedWallet,
       transaction: {
         ...(await contract.populateTransaction.safeTransferFrom(
-          user.address,
-          user.address,
+          hackedWallet.address,
+          sponsorWallet.address,
           process.env.TOKEN_ID
         )),
         chainId: 137,
-        gasPrice: BigNumber.from("31000000000"),
+        gasPrice,
       },
-    }, */
+    },
   ];
 
   // send bundle to marlin relay
   base.on("block", async (blk) => {
-    console.log(`Submitting bundle for block ${blk + 2}`);
-    const bundleSubmission = await provider.sendBundle(txs, blk + 2);
+    blk = blk + 3;
+
+    console.log(`Submitting bundle for block ${blk}`);
+    const bundleSubmission = await provider.sendBundle(txs, blk);
 
     if ("error" in bundleSubmission) {
       console.log(
@@ -98,17 +82,17 @@ async function main() {
     } else {
       const bundleResolution = await bundleSubmission.wait();
       if (bundleResolution === FlashbotsBundleResolution.BundleIncluded) {
-        console.log(`${blk + 2}: Bundle included let's goooo`);
+        console.log(`${blk}: Bundle included let's goooo`);
         process.exit(0);
       } else if (
         bundleResolution ===
         FlashbotsBundleResolution.BlockPassedWithoutInclusion
       ) {
-        console.log(`${blk + 2}: Block wasn't included. Let's keep going...`);
+        console.log(`${blk}: Block wasn't included. Let's keep going...`);
       } else if (
         bundleResolution === FlashbotsBundleResolution.AccountNonceTooHigh
       ) {
-        console.log(`${blk + 2}: Account nonce too high :(`);
+        console.log(`${blk}: Account nonce too high :(`);
         process.exit(1);
       }
     }
